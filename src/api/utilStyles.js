e135f3ed5b23303ac7d1de760b6552ee36717a7b
Caleb Nance <calebnance@gmail.com>
2018-10-12 12:12:16 -0400

    utilStyles updated and used

--- src/api/utilStyles.js
+++ src/api/utilStyles.js
@@ -1,3 +1,4 @@
+import { StyleSheet } from 'react-native';
 import colors from './colors';
 
 // utility styles
@@ -5,15 +6,32 @@ import colors from './colors';
 export default {
   btn: {
     alignItems: 'center',
-    backgroundColor: colors.white50,
-    borderRadius: 20,
+    backgroundColor: colors.white,
+    borderColor: colors.brandPrimary,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 80,
     paddingHorizontal: 24,
     paddingVertical: 8
+  },
+  btnText: {
+    color: colors.brandPrimary,
+    textAlign: 'center'
+  },
+  container: {
+    backgroundColor: colors.white,
+    flex: 1
+  },
+  contentContainer: {
+    alignItems: 'center',
+    paddingTop: 30
   },
   opacityActive: 0.8,
   opacityActiveFull: 1,
   spacer80: {
     height: 80,
     width: '100%'
+  },
+  text: {
+    color: colors.brandPrimary
   }
 };
