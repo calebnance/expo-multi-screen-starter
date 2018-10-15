6a1cbbff593ea6b5a7a27dcfd0a85d447a62670b
Caleb Nance <calebnance@gmail.com>
2018-10-15 14:23:48 -0400

    custom svg navigation back button added (accessible)

---
+++ src/components/NavigationBack.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import { utilStyles } from '../api/constants';
+
+// grab svg icon
+import SvgCircleLeft from './icons/Svg.CircleLeft';
+
+const NavigationBack = props => {
+  const { navigation } = props;
+
+  return (
+    <TouchableOpacity
+      accessible
+      accessibilityLabel="go back"
+      accessibilityComponentType="button"
+      accessibilityTraits="button"
+      activeOpacity={utilStyles.opacityActive}
+      onPress={() => navigation.goBack(navigation.state.key)}
+      style={{ padding: 12 }}
+    >
+      <SvgCircleLeft active />
+    </TouchableOpacity>
+  );
+};
+
+// type checking
+NavigationBack.propTypes = {
+  // required
+  navigation: PropTypes.object.isRequired
+};
+
+export default NavigationBack;
--- src/screens/MultiLevel2Screen.js
+++ src/screens/MultiLevel2Screen.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { ScrollView, Text, View } from 'react-native';
 import { utilStyles } from '../api/constants';
 
+// grab component
+import NavigationBack from '../components/NavigationBack';
+
 const MultiLevel2Screen = () => (
   <View style={utilStyles.container}>
     <ScrollView contentContainerStyle={utilStyles.contentContainer}>
@@ -10,13 +13,14 @@ const MultiLevel2Screen = () => (
   </View>
 );
 
-MultiLevel2Screen.navigationOptions = {
+MultiLevel2Screen.navigationOptions = ({ navigation }) => ({
+  headerLeft: <NavigationBack navigation={navigation} />,
   headerRight: <View style={{ flex: 1 }} />,
   headerTitleStyle: {
     flex: 1,
     textAlign: 'center'
   },
   title: 'Level 2'
-};
+});
 
 export default MultiLevel2Screen;
