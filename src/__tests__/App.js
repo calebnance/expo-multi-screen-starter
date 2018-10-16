648b20cbe4ed773f326c5435f3be8440388bf06f
Caleb Nance <calebnance@gmail.com>
2018-10-16 12:08:24 -0400

    adding enzyme to the project and updated tests

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import App from '../../App';
 
-it('App renders correctly', () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
+const app = shallow(<App />);
+
+describe('<App />', () => {
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
+});
+
+describe('<App /> State', () => {
+  it('state: isLoading = true', () => {
+    // initial state: isLoading
+    expect(app.state('isLoading')).toEqual(true);
+    expect(app).toMatchSnapshot();
+  });
+
+  it('state: isLoading = false', () => {
+    // mock isLoading completed
+    app.setState({
+      isLoading: false
+    });
+
+    expect(app).toMatchSnapshot();
+  });
 });
