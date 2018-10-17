75d8cc9a346f2403edabfb3d20bdb3eabc5fcfa4
Caleb Nance <calebnance@gmail.com>
2018-10-17 07:16:24 -0400

    component tests updated

--- src/__tests__/components/NavigationBack.js
+++ src/__tests__/components/NavigationBack.js
@@ -3,11 +3,21 @@ import { shallow } from 'enzyme';
 
 import NavigationBack from '../../components/NavigationBack';
 
-const navigation = { navigate: jest.fn() };
+const navigation = {
+  goBack: jest.fn(),
+  navigate: jest.fn(),
+  state: {
+    key: 1
+  }
+};
 const component = shallow(<NavigationBack navigation={navigation} />);
 
 describe('<NavigationBack />', () => {
   it('renders correctly', () => {
     expect(component).toMatchSnapshot();
+  });
+
+  it('onPress', () => {
+    component.props().onPress();
   });
 });
--- src/__tests__/components/Touch.js
+++ src/__tests__/components/Touch.js
@@ -3,10 +3,13 @@ import { shallow } from 'enzyme';
 
 import Touch from '../../components/Touch';
 
-const component = shallow(<Touch onPress={() => jest.fn()} />);
-
 describe('<Touch />', () => {
   it('renders correctly', () => {
+    const onPressEvent = jest.fn();
+    onPressEvent.mockReturnValue('Touch on press invoked');
+
+    const component = shallow(<Touch onPress={onPressEvent} />);
+
     expect(component).toMatchSnapshot();
   });
 });
