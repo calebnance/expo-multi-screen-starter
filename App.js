e36f36e8f4b84c931e73cb0614819b134850e7ee
Caleb Nance <calebnance@gmail.com>
2019-06-13 22:59:05 -0400

    remove lottie intro animation

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import { Animated, Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, DangerZone, ScreenOrientation } from 'expo';
-import { colors, device, func, gStyle } from './src/constants';
+import { Platform, StatusBar, View } from 'react-native';
+import { AppLoading, ScreenOrientation } from 'expo';
+import { device, func, gStyle } from './src/constants';
 
 // navigation switch
 import AppSwitchNav from './src/navigation/AppSwitchNav';
-
-// danger zone
-const { Lottie } = DangerZone;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      introAnimation: null,
-      isLoading: true,
-      showSplash: true,
-      splashOpacity: new Animated.Value(1),
-      tries: 5
+      isLoading: true
     };
 
     // iPad? (TODO in future android tablet checked)
@@ -27,61 +20,10 @@ class App extends React.Component {
         ScreenOrientation.Orientation.ALL_BUT_UPSIDE_DOWN
       );
     }
-
-    this.loadAnimationAsync = this.loadAnimationAsync.bind(this);
-    this.playAnimation = this.playAnimation.bind(this);
-  }
-
-  componentDidMount() {
-    this.loadAnimationAsync();
-  }
-
-  async loadAnimationAsync() {
-    const path =
-      'https://s3.amazonaws.com/calebnance/react-native/animations/diamond.json';
-    const response = await fetch(path);
-    const json = await response.json();
-
-    this.setState({ introAnimation: json }, this.playAnimation);
-  }
-
-  playAnimation() {
-    const { introAnimation, splashOpacity, tries } = this.state;
-
-    // make sure animation json has loaded
-    if ((!introAnimation || this.animation === undefined) && tries >= 1) {
-      this.setState(
-        {
-          tries: tries - 1
-        },
-        () => {
-          setTimeout(() => {
-            this.playAnimation();
-          }, 0);
-        }
-      );
-      return;
-    }
-
-    // reset and start animation
-    this.animation.reset();
-    this.animation.play();
-
-    // start to fade the animation out before it's completely finished
-    setTimeout(() => {
-      Animated.timing(splashOpacity, {
-        toValue: 0
-      }).start(() => {
-        // console.log('animation finished');
-        this.setState({
-          showSplash: false
-        });
-      });
-    }, 1400);
   }
 
   render() {
-    const { introAnimation, isLoading, showSplash, splashOpacity } = this.state;
+    const { isLoading } = this.state;
 
     if (isLoading) {
       return (
@@ -97,44 +39,11 @@ class App extends React.Component {
         <StatusBar
           barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'}
         />
-        {showSplash && (
-          <Animated.View
-            style={[styles.containerSplash, { opacity: splashOpacity }]}
-          >
-            <View style={styles.alignCenter}>
-              <Lottie
-                ref={animation => {
-                  this.animation = animation;
-                }}
-                loop={false}
-                style={styles.lottie}
-                source={introAnimation}
-              />
-            </View>
-          </Animated.View>
-        )}
 
-        {!showSplash && <AppSwitchNav />}
+        <AppSwitchNav />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  containerSplash: {
-    backgroundColor: colors.brandPrimary,
-    height: '100%',
-    justifyContent: 'center',
-    position: 'absolute',
-    width: '100%'
-  },
-  alignCenter: {
-    alignSelf: 'center'
-  },
-  lottie: {
-    height: device.height,
-    width: device.width
-  }
-});
 
 export default App;
