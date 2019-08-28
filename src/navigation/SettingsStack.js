117e15add9946fa771c8a8389c40a95cd885ecb5
Caleb Nance <calebnance@gmail.com>
2019-08-28 01:01:16 -0400

    broke navigation stacks out into separate files

---
+++ src/navigation/SettingsStack.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createStackNavigator } from 'react-navigation';
+
+// screens
+import SettingsScreen from '../screens/SettingsScreen';
+
+// icons
+import SvgCog from '../components/icons/Svg.Cog';
+
+const SettingsTabBarIcon = ({ focused }) => <SvgCog active={focused} />;
+SettingsTabBarIcon.propTypes = {
+  // required
+  focused: PropTypes.bool.isRequired
+};
+
+// Settings Stack
+// /////////////////////////////////////////////////////////////////////////////
+const SettingsStack = createStackNavigator(
+  {
+    Settings: SettingsScreen
+  },
+  {
+    navigationOptions: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: SettingsTabBarIcon
+    }
+  }
+);
+
+export default SettingsStack;
---
+++ src/navigation/StatsStack.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createStackNavigator } from 'react-navigation';
+
+// screens
+import StatsScreen from '../screens/StatsScreen';
+
+// icons
+import SvgStats from '../components/icons/Svg.Stats';
+
+const StatsTabBarIcon = ({ focused }) => <SvgStats active={focused} />;
+StatsTabBarIcon.propTypes = {
+  // required
+  focused: PropTypes.bool.isRequired
+};
+
+// Stats Stack
+// /////////////////////////////////////////////////////////////////////////////
+const StatsStack = createStackNavigator(
+  {
+    Stats: StatsScreen
+  },
+  {
+    navigationOptions: {
+      tabBarLabel: 'Stats',
+      tabBarIcon: StatsTabBarIcon
+    }
+  }
+);
+
+export default StatsStack;
